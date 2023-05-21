9655d06e202ab556ab6a2e69c139e3f8329aa2dd
Antonio Manuel <anmamebo2001@gmail.com>
2023-05-21 20:14:19 +0200

    Añadidas rutas scss a vite.config.js

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -4,7 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.js', 'resources/css/home.scss'],
+            input: [
+                'resources/css/app.scss',
+                'resources/js/app.js',
+                'resources/css/admin.scss',
+                'resources/css/auth.scss',
+                'resources/css/cart.scss',
+                'resources/css/home.scss',
+                'resources/css/info.scss',
+                'resources/css/product.scss',
+                'resources/css/products.scss',
+                'resources/css/profile.scss',
+                'resources/css/wish-list.scss',
+            ],
             refresh: true,
         }),
     ],
